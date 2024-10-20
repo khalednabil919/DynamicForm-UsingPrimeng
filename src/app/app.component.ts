9a967fa59d9e941cdf7a9304e970bd4c0d8f34fa
Khaled.Nabil <Khaled.Nabil@KHALED_NABIL>
2024-10-20 14:31:34 +0300

    DynamicForm-Using-Primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './app.service';
+import { FormField, FormFieldJSON } from './form-fields';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'DynamicForm-UsingPrimeng';
+  formFields: FormFieldJSON [] = [];
+  dynamicForm = this.fb.group({});
+  constructor(private appService:AppService,private fb:FormBuilder){}
+  ngOnInit()
+  {
+    this.getDynamicFormFields();
+  }
+  getDynamicFormFields(){
+    this.appService.getFormFields().subscribe((response:FormField)=>{
+      console.log(response);
+      this.formFields = response.data;
+      this.setDynamicForm(this.formFields);
+    })
+  }
+  setDynamicForm(controls:FormFieldJSON[])
+  {
+    for(const control of controls)
+    {
+      const validators = [];
+      if(control.validators?.required)
+      {
+        validators.push(Validators.required);
+      }
+      this.dynamicForm.addControl(control.name, this.fb.control(control.value,validators));
+    }
+  }
+  saveForm(){
+    console.log(this.dynamicForm.value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations' 
+import { CardModule } from 'primeng/card';
+import{HttpClientModule} from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { DropdownModule } from 'primeng/dropdown';
 
 @NgModule({
   declarations: [
@@ -10,7 +19,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CardModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    ButtonModule,
+    InputTextModule,
+    InputNumberModule,
+    DropdownModule,
+    InputTextareaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
